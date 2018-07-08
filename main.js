15a59b0d2f9a63e81b8043293903ac5f4d7d9248
mdroberts74 <mikedroberts88@gmail.com>
2018-07-08 10:54:53 -0500

    I added a varible called page counter equals 1 to make the link to my API dynamic. So every time you click the link will incriment by one.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+//I created a varible to start at 1 so that the AJAX link will load page 1, then 2, then 3. To load a new page on click.
+let pageCounter = 1;
+
 // I created a varible that will refer to the ID in my main div called aninmal-info.
 const animalContainer = document.getElementById("animal-info");
 // I created a variable that refers to the button id called btn.
@@ -8,7 +11,8 @@ btn.addEventListener("click", function() {
 
     //I created a variable that can request data from the web server with XMLHttpRequest.
     let ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+    //At the end of this line I removed the 1 and created a varible called page counter that equals 1. So the link will first see 1, then on click it will increment by 1 loading the next API.
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 
     // This is waits until the page loads then JSON.parse will convert the data from a string to an object.
     ourRequest.onload = function() {
@@ -16,11 +20,13 @@ btn.addEventListener("click", function() {
         renderHTML(ourData);
     };
     ourRequest.send();
+    //This will increment the AJAX link by one. 
+    pageCounter++;
 
 });
 // This function adds HTML to the DOM.
 function renderHTML(data) {
-
+    // I created a varible that holds a blank string. I created a loop to loop through my array, then I am calling my variable and concatenation a string with my array.
     let htmlString = "";
     for (i = 0; i < data.length; i++) {
         htmlString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
